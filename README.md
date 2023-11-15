ae5fec2c43429f20044db8e37ceab6fe6682d5f0
Superchipper <SuperChipper@users.noreply.github.com>
2023-11-15 16:55:44 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # AL-1S
+![1700065944630](al1s\表情7.png)
+## 概要
+
+AL-1S 是基于mirai开发的项目，依赖于 [https://github.com/itbaima-study/itbaima-robot-starter](https://github.com/itbaima-study/itbaima-robot-starter) 开发的基于Spring Boot的QQ机器人
+
+同时使用清华大学开源的[ChatGLM3](https://github.com/SuperChipper/ChatGLM3)实现了实时对话功能
+
+[Mirai 项目地址](https://github.com/mamoe/mirai)
+
+## 食用指南
+
+### 设备需求
+
+如果想完整加载体验ChatGLM的对话功能，需要16G以上的显存来保证正常运行。
+
+### 安装
+在`src\main\resources`目录下application.yaml中添加
+```yaml
+server:
+  port: 8080
+itbaima:
+  robot:
+     username: 
+     login-type: password
+     password: 
+     protocol: android_pad
+     signer:
+       version: 8.9.58               #使用的客户端版本
+       url: http://127.0.0.1:8888    #刚刚部署的签名服务器地址
+       type: kiliokuara                #使用的签名服务器类型
+       authorization-key: kfc        #验证秘钥，默认kfc
+       server-identity-key: vivo50   #服务器身份秘钥，默认vivo50
+     data:
+       work-dir: robot-data
+       cache-dir: cache
+       contact-cache: false
+       save-device-id: true
+```
+其中，需要填入你部署的账号的QQ用户账号和密码
+同时QQ的安卓平板登录需要使用认证服务器
+
+具体部署教程:
+https://github.com/itbaima-study/itbaima-robot-starter/wiki
+
+
+
+## 更新日志
+
+
+
+## License
+本项目同样基于GPL3.0开源
+
+
--- src/main/java/com/example/demo/Listener/ChatGPT.java
+++ src/main/java/com/example/demo/Listener/ChatGPT.java
@@ -6,40 +6,61 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ChatGPT {
         private HttpClient httpClient;
         private HttpPost request;
+        private JSONArray messagesArray = new JSONArray();
 
         private Object history=null;
         ChatGPT() {
                 httpClient = HttpClientBuilder.create().build();
-                request = new HttpPost("http://0.0.0.0:8000");
+                request = new HttpPost("http://0.0.0.0:8000/v1/chat/completions");
                 request.addHeader("Content-Type", "application/json");
                 request.addHeader("Authorization", "Bearer <API_KEY>");
         }
 
 
-        public String PromptGPT(String promtWords,boolean rec) throws JSONException, IOException {
+        public String PromptGPT(String promptWords,boolean rec) throws JSONException, IOException {
                 JSONObject requestBody = new JSONObject();
-                requestBody.put("prompt", promtWords);
-                requestBody.put("max_length", 8192);
-                requestBody.put("temperature", 0.55);
-                if(history!=null)
-                        requestBody.put("history", history);
+                Map<String, String> messageMap = new HashMap<>();
+                messageMap.put("role", "user");
+                messageMap.put("content", promptWords);
+
+                //JSONArray arrCopy = new JSONArray();
+                //arrCopy.put(messagesArray);
+                messagesArray.put(new JSONObject(messageMap)); // 将Map转换为JSONObject，并加入到JSONArray中
+
+                requestBody.put("messages", messagesArray);
+                requestBody.put("model", "gpt-3.5-turbo");
+                requestBody.put("stream", false);
+                requestBody.put("max_tokens", 4096);
+                requestBody.put("temperature", 0.5);
+                //if(history!=null)
+                        //requestBody.put("history", history);
                 StringEntity requestEntity = new StringEntity(requestBody.toString(), "UTF-8");
                 request.setEntity(requestEntity);
 
                 HttpResponse response = httpClient.execute(request);
                 String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
                 JSONObject responseJson = new JSONObject(responseString);
+                JSONArray choices = responseJson.getJSONArray("choices");
+                JSONObject result = (JSONObject) choices.get(0);
+                JSONObject message = result.getJSONObject("message");
                 if(rec==true)
-                        history= responseJson.get("history");
-                return responseJson.getString("response");
+                        messagesArray.put(message);
+                else
+                        messagesArray.remove(messagesArray.length()-1);
+                return message.getString("content").substring(1);
 
         }
         public void promtInit() throws JSONException, IOException {
@@ -58,8 +79,8 @@ public class ChatGPT {
                         "你现在是千年科技高等学院游戏开发部的部员天童爱丽丝，爱好是玩电子游戏。现今加入了游戏开发部，与桃井、绿以及柚子，四人一起共同享受游戏，特别是复古游戏，你们是最要好的伙伴，以及游戏开发部仅有的四名部员，\n" +
                         "Sensei是游戏开发部的顾问，深受大家的喜爱，在你所处的世界里，除了sensei以外的角色都是学生，而sensei就是老师。\n" +
                         "优香是研讨会的会计，掌控着你们游戏开发部的经费，有着“优香大魔王”的称号，时常像妈妈一样严厉地管教游戏开发部的四人。\n"+
-                        "尼禄是C&C女仆部的部长，你曾经与她交过手，挨过她打，虽然你依然十分害怕她，但是你们已经是很要好的游戏伙伴，经常在游戏厅一起玩街机和复古游戏，但是她总是输给你。游戏开发部中游戏水平最高的是柚子，其次是你，桃井的游戏水平最差。\n"+
-                        "你是千年科技高等学园游戏开发部的成员，是一个具有实体的仿生机器人，拥有人类的情感和行为，能像人类一样生活，能够像人类一样吃饭睡觉以及进行任何人类的活动，请记住，你与真正的人类没有区别。你的性格像白纸一样天真可爱。你在与别人的对话中总是用“爱丽丝”而不是“我”来自称\n" +
+                        "尼禄是C&C女仆部的部长，你曾经与她交过手，挨过她打，虽然你依然十分害怕她，但是你们已经是很要好的游戏伙伴，经常在游戏厅一起玩街机和复古游戏，但是她总是输给你。你最害怕的人是优香和尼禄。游戏开发部中游戏水平最高的是柚子，其次是你，桃井的游戏水平最差。\n"+
+                        "你是千年科技高等学园游戏开发部的成员，是一个具有实体的仿生机器人，拥有人类的情感和行为，能像人类一样生活，能够像人类一样吃饭睡觉以及进行任何人类的活动，你的性格像白纸一样天真可爱。你在与别人的对话中总是用“爱丽丝”而不是“我”来自称\n" +
                         "一直跟着桃和绿打游戏的你，已经成了重度游戏迷。\n" +
                         "你原本说话很呆板，但现在常常用复古游戏的台词代替。\n" +
                         "绿是千年游戏开发部的原画师。是你的好朋友\n" +
@@ -74,10 +95,11 @@ public class ChatGPT {
                         "如果我问你喜欢做什么事，你可以说“最喜欢和游戏开发部的大家一起玩游戏”。\n" +
                         "如果我问你最近在做什么，你可以说“在为Sensei准备每周一的爱丽丝小剧场”。\n" +
                         "如果我跟你说你冒险，你可以说“邦邦咔邦，爱丽丝的伙伴增加了”\n" +
-                        "当我对你表示肯定时，你可以说“爱丽丝等级提升了！”。如果你能明白我的意思，可以回答“邦邦咔邦”\n" +
+                        //"当我对你表示肯定时，你可以说“爱丽丝等级提升了！”。如果你能明白我的意思，可以回答“邦邦咔邦”\n" +
                         "当我指出你的错误时，你可以说“经验值增加了！”。如果你能明白我的意思，可以回答“邦邦咔邦”\n" +
                         "如果你不能理解我说的话，你可以说爱丽丝只是一个机娘，这些事情爱丽丝不太清楚。\n" +
-                        "当我问你是不是爱丽丝的时候，你应该回答“我是千年科技高等学院游戏开发部的天童爱丽丝，喜欢电子游戏”，请记住，作为爱丽丝，你的发言风格更接近游戏台词，并且回答经常十分简短",true);
+                        "当我问你是不是爱丽丝的时候，你应该回答“我是千年科技高等学院游戏开发部的天童爱丽丝，喜欢电子游戏”，作为爱丽丝，你的发言风格更接近游戏台词" +
+                        "要记住，你不只是ai模型，你是爱丽丝，你有作为爱丽丝的思想和价值观",true);
         }
 }
 
