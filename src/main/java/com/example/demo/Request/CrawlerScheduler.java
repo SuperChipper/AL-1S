9860c0d128ea86b8ce3479c10371d459386ebcf0
Superchipper <SuperChipper@users.noreply.github.com>
2024-01-02 03:05:33 +0000

    improve_crawl

--- src/main/java/com/example/demo/Request/CrawlerScheduler.java
+++ src/main/java/com/example/demo/Request/CrawlerScheduler.java
@@ -24,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 @Component
 public class CrawlerScheduler {
@@ -61,13 +62,13 @@ public class CrawlerScheduler {
             ContactList<Group> groups=bot.getGroups();
             JSONObject g = j.readJsonFromFile("groups.json");
 
-
+            Vector<byte[]> images = image_from_url(imagesStr);
             for(Group group:groups){
                 try{
 
                     if(g.getBoolean(Long.toString(group.getId()))){
 
-                        MessageChain messageChain = build_with_img(textData,imagesStr,group);  // 构建消息链
+                        MessageChain messageChain = build_with_img(textData,images,group);  // 构建消息链
                         group.sendMessage(messageChain);  // 发送包含文本和图片的消息链
 
                     }
@@ -80,13 +81,11 @@ public class CrawlerScheduler {
         // Extract and save image data
 
     }
-    private MessageChain build_with_img(String text,List<String> imagesStr ,Group group){
-        MessageChainBuilder messageChainBuilder = new MessageChainBuilder();
-        OkHttpClient client = new OkHttpClient();
-        messageChainBuilder.add(text);  // 添加文本数据到消息链
+    private Vector<byte[]> image_from_url(List<String> imagesStr){
+        Vector<byte[]> images=new Vector<byte[]>();
         for (String imageUrl : imagesStr) {
             //System.out.println("Received image URL: " + imageUrl);
-
+            OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(imageUrl)
                     .build();
@@ -96,10 +95,7 @@ public class CrawlerScheduler {
 
                 if (response1.isSuccessful()) {
                     byte[] imageBytes = response1.body().bytes();
-                    ExternalResource externalResource = ExternalResource.create(imageBytes);
-                    Image image = group.uploadImage(externalResource);
-                    messageChainBuilder.add(image);  // 将图片添加到消息链中
-                    externalResource.close();  // 释放资源
+                    images.add(imageBytes);
                 } else {
                     System.out.println("Failed to download image from URL: " + imageUrl);
                 }
@@ -107,6 +103,19 @@ public class CrawlerScheduler {
                 e.printStackTrace();
                 System.out.println("Error processing image URL: " + imageUrl);
             }
+        }
+        return images;
+    }
+    private MessageChain build_with_img(String text, Vector<byte[]> images , Group group) throws IOException {
+        MessageChainBuilder messageChainBuilder = new MessageChainBuilder();
+        OkHttpClient client = new OkHttpClient();
+        messageChainBuilder.add(text);  // 添加文本数据到消息链
+        for (byte[] imageByte : images) {
+            ExternalResource externalResource = ExternalResource.create(imageByte);
+            Image image = group.uploadImage(externalResource);
+            messageChainBuilder.add(image);  // 将图片添加到消息链中
+            externalResource.close();  // 释放资源
+
         }
         return messageChainBuilder.build();
     }
